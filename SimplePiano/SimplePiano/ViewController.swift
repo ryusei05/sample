aa2f0df42b7a595b61b54b30f5f98e657eddd9fb
Ryusei Maruyama <r.maruyama@pro-japan.co.jp>
2016-11-17 18:12:44 +0900

    ボタンをタップしたら音楽を流すサンプルアプリ

---
+++ SimplePiano/SimplePiano/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimplePiano
+//
+//  Created by 丸山 隆清 on 2016/11/17.
+//  Copyright © 2016年 Ryusei Maruyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
